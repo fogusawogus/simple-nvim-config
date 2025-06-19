a479c6e3312c7927ad0adb2e1b203463742050e8
Jacob W <222jacobw@gmail.com>
2025-06-18 20:45:50 -0500

    More like dired

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -4,6 +4,16 @@ return {
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {},
   config = function()
+    function _G.get_oil_winbar()
+      local bufnr = vim.api.nvim_win_get_buf(vim.g.statusline_winid)
+      local dir = require('oil').get_current_dir(bufnr)
+      if dir then
+        return vim.fn.fnamemodify(dir, ':~')
+      else
+        -- If there is no current directory (e.g. over ssh), just show the buffer name
+        return vim.api.nvim_buf_get_name(0)
+      end
+    end
     require('oil').setup {
       columns = {
         'icon',
@@ -15,6 +25,10 @@ return {
         show_hidden = true,
       },
       skip_confirm_for_simple_edits = true,
+      watch_for_changes = true,
+      win_options = {
+        winbar = '%!v:lua.get_oil_winbar()',
+      },
     }
     vim.keymap.set('n', '<leader>e', function()
       vim.cmd 'Oil'
